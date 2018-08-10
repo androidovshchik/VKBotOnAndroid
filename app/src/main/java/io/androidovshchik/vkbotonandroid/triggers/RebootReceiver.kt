4937f7071c42c5ce11ec5192e025a44804cbbd57
vlad <gimrcpp@gmail.com>
2018-08-10 19:48:58 +0300

    changes

---
+++ app/src/main/java/io/androidovshchik/vkbotonandroid/triggers/RebootReceiver.kt
@@ -0,0 +1,18 @@
+package io.androidovshchik.vkbotonandroid.triggers
+
+import android.annotation.SuppressLint
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import com.github.androidovshchik.core.utils.context.forceRestartForegroundService
+import io.androidovshchik.vkbotonandroid.VKService
+import timber.log.Timber
+
+class RebootReceiver : BroadcastReceiver() {
+
+    @SuppressLint("UnsafeProtectedBroadcastReceiver")
+    override fun onReceive(context: Context, intent: Intent?) {
+        Timber.d("RebootReceiver")
+        context.forceRestartForegroundService(VKService::class.java)
+    }
+}
